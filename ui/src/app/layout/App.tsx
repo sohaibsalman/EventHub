04b22f8dc5df7b114275eee581d96d284f6035b0
Sohaib Salman <sohaibsalman10@gmail.com>
2020-12-15 12:57:17 +0500

    Add crud operations to react app

---
+++ ui/src/app/layout/App.tsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState, Fragment } from "react";
+import axios from "axios";
+
+import NavBar from "../../features/nav/NavBar";
+
+import { IActivity } from "../models/activity";
+import { Container } from "semantic-ui-react";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
+    null
+  );
+  const [editMode, setEditMode] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get<IActivity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        let activities = [];
+        response.data.forEach((a) => {
+          a.date = a.date.split(".")[0];
+          activities.push(a);
+        });
+        setActivities(response.data);
+      });
+  }, []);
+
+  const handleSelectedActivity = (id: string) => {
+    setSelectedActivity(activities.filter((a) => a.id === id)[0]);
+    setEditMode(false);
+  };
+
+  const handleOpenCreateActivity = () => {
+    setSelectedActivity(null);
+    setEditMode(true);
+  };
+
+  const handleCreateActivity = (activity: IActivity) => {
+    setActivities([...activities, activity]);
+    setSelectedActivity(activity);
+    setEditMode(false);
+  };
+
+  const handleEditActivity = (activity: IActivity) => {
+    setActivities([
+      ...activities.filter((a) => a.id !== activity.id),
+      activity,
+    ]);
+    setSelectedActivity(activity);
+    setEditMode(false);
+  };
+
+  const handleDeleteActivity = (id: string) => {
+    setActivities([...activities.filter((a) => a.id !== id)]);
+  };
+
+  return (
+    <Fragment>
+      <NavBar openCreateActivity={handleOpenCreateActivity} />
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard
+          activities={activities}
+          selectActivity={handleSelectedActivity}
+          selectedActivity={selectedActivity}
+          editMode={editMode}
+          setEditMode={setEditMode}
+          setSelectedActivity={setSelectedActivity}
+          createActivity={handleCreateActivity}
+          editActivity={handleEditActivity}
+          deleteActivity={handleDeleteActivity}
+        />
+      </Container>
+    </Fragment>
+  );
+};
+
+export default App;
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 
 import "semantic-ui-css/semantic.css";
-import './index.css';
+import './app/layout/styles.css';
 
 ReactDOM.render(
   <React.StrictMode>
